327c6fc652bb76b0a2aacc4025050f2efeda88f9
shrey-creator <60284082+shrey-creator@users.noreply.github.com>
2020-11-27 20:28:50 +0530

    make things better

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express =require('express');
 const app =express();
 const socket=require('socket.io');
 app.use(express.static('join'));
+app.use(express.static('room'));
 app.use(express.static('board'));
 
 app.use(express.static('slip'));
-//app.use(express.static('room'));
+
 var passedNumber=[];
 var number=[];
 var server=app.listen((process.env.PORT || 5000),()=>{console.log("app started on localhost 5000")});
